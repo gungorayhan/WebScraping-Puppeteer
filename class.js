25fecc534b2f05138a1ff29b2e8fc1a46c1d6778
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2024-03-20 11:29:05 +0300

    Pagination & saving data to csv file

---
+++ class.js
@@ -0,0 +1,20 @@
+// const puppeteer = require("puppeteer")
+
+// async function app() {
+//     const browser = await puppeteer.launch({ headless: false })
+//     const page = await browser.newPage();
+//     await page.goto("https://www.amazon.com/", {
+//         waitUntil: load
+//     })
+
+//     const is_disabled = await page.$("li.a-disabled.a-last") !== null;
+
+//     console.log(is_disabled)
+
+//     await browser.close();
+// }
+
+
+// app()
+
+//it writed to try in page
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer")
-
+const fs = require("fs")
 async function app() {
     const browser = await puppeteer.launch({
         headless: false, // browser will open
@@ -9,54 +9,81 @@ async function app() {
     const page = await browser.newPage()
     await page.goto("https://www.amazon.com/s?rh=n%3A16225007011&fs=true&ref=lp_16225007011_sar")
 
-    const productsHandles = await page.$$("div.s-main-slot.s-result-list.s-search-results.sg-row")
 
-
-    let i =0;
+    let i = 0;
 
     let items = [];
+    let isBtnDisabled = false;
 
-    for (const productHandle of productsHandles) {
-        let title = "Null"
-        let price = "Null"
-        let image = "Null"
+    while (!isBtnDisabled) {
 
-        try {
-               title = await page.evaluate(
-                el => el.querySelector("h2 > a > span").textContent,
-                productHandle)
-            // console.log(title)
-        } catch (error) {
+        await page.waitForSelector('[data-cel-widget="search_result_0"]');
+        const productsHandles = await page.$$("div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item")
+
+        for (const productHandle of productsHandles) {
+
+            let title = "Null"
+            let price = "Null"
+            let image = "Null"
+
+            try {
+                title = await page.evaluate(
+                    el => el.querySelector("h2 > a > span").textContent,
+                    productHandle)
+                // console.log(title)
+            } catch (error) {
+
+            }
+
+            try {
+                price = await page.evaluate(
+                    (el) => el.querySelector(".a-price> .a-offscreen").textContent,
+                    productHandle
+                )
+                // console.log(price)
+            } catch (error) {
+
+            }
+
+            try {
+                image = await page.evaluate(
+                    (el) => el.querySelector(".s-image").getAttribute("src"),
+                    productHandle
+                )
+                // console.log(image)
+            } catch (error) {
+
+            }
+
+
             
+            if (title !== "Null") {
+                console.log(title, price, image)
+                items.push({ title, price, image })
+
+                fs.appendFile("result.cvs",
+                `${title.replace(/,/g, ".")},${price},${image}\n`,(err)=>{
+                    if(err) throw err
+                    console.log("saved!")
+                })
+            }
         }
 
-        try {
-              price = await page.evaluate(
-                (el) => el.querySelector(".a-price> .a-offscreen").textContent,
-                productHandle
-            )
-            // console.log(price)
-        } catch (error) {
-            
+        await page.waitForSelector("li.a-last"), { visible: true }
+
+        const is_disabled = (await page.$("li.a-disabled.a-last")) !== null;
+
+        isBtnDisabled = is_disabled
+        if (!is_disabled) {
+            await Promise.all([
+                page.click("li.a-last"),
+                page.waitForNavigation({waitUntil:"networkidle2"})
+            ])
         }
-
-        try {
-             image = await page.evaluate(
-                (el)=>el.querySelector(".s-image").getAttribute("src"),
-                productHandle
-            )
-            // console.log(image)
-        } catch (error) {
-
-        }
-
-
-        console.log(title,price,image)
-        items.push({title,price,image})
     }
 
-    console.log(items.length)
-    console.log(items)
+    // console.log(items.length)
+    // console.log(items)
 
     await browser.close()
 }
